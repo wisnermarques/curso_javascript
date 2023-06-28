1f7ab3034a40ca8b5466bf7e2703b572405d3340
Wisner Marques <wisner.a.marques@gmail.com>
2023-06-28 11:48:29 -0300

    first commit

---
+++ test.js
@@ -0,0 +1,9 @@
+function soma(e) {
+  e.preventDefault();
+  const valor1 = parseFloat(document.getElementById("valor1").value);
+  const valor2 = parseFloat(document.getElementById("valor2").value);
+  const soma = valor1 + valor2;
+  let resultadoElement = document.getElementById("resultado");
+
+  resultadoElement.textContent = "A soma é " + soma;
+}
