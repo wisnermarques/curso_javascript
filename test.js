4c245e48c6aae881025dd207e51f9c1127ec2469
Wisner Marques <wisner.a.marques@gmail.com>
2023-08-04 08:20:01 -0300

    teste radio

--- test.js
+++ test.js
@@ -1,39 +1,50 @@
 function soma(e) {
-  e.preventDefault();
-  const valor1 = parseFloat(document.getElementById("valor1").value);
-  const valor2 = parseFloat(document.getElementById("valor2").value);
+  e.preventDefault()
+  const valor1 = parseFloat(document.getElementById('valor1').value)
+  const valor2 = parseFloat(document.getElementById('valor2').value)
 
-  const op = document.getElementById("op").value;
+  const op = document.getElementById('op').value
 
-  let resultado;
+  let resultado
 
-  let resultadoElement = document.getElementById("resultado");
+  let resultadoElement = document.getElementById('resultado')
 
   switch (op) {
-    case "sum":
-      resultado = valor1 + valor2;
-      break;
-    case "sub":
-      resultado = valor1 - valor2;
-      break;
-    case "mult":
-      resultado = valor1 * valor2;
-      break;
-    case "div":
+    case 'sum':
+      resultado = valor1 + valor2
+      break
+    case 'sub':
+      resultado = valor1 - valor2
+      break
+    case 'mult':
+      resultado = valor1 * valor2
+      break
+    case 'div':
       if (!isNaN(valor2) && valor2 !== 0) {
-        resultado = valor1 / valor2;
+        resultado = valor1 / valor2
       } else {
-        resultadoElement.textContent = "Não é permitido divisão por zero.";
+        resultadoElement.textContent = 'Não é permitido divisão por zero.'
       }
-      break;
+      break
     default:
-      resultadoElement.textContent = "Operação não selecionada.";
+      resultadoElement.textContent = 'Operação não selecionada.'
   }
 
-  resultadoElement.textContent = resultado.toFixed(2);
+  resultadoElement.textContent = resultado.toFixed(2)
 }
 
 function limpar() {
-  let resultadoElement = document.getElementById("resultado");
-  resultadoElement.textContent = "";
+  let resultadoElement = document.getElementById('resultado')
+  resultadoElement.textContent = ''
+}
+
+function testar() {
+  const  sexoM = document.getElementById('masc')
+  const  sexoF = document.getElementById('fem')
+  if(sexoM.checked) {
+    document.getElementById('result').innerHTML = 'M'
+  }
+  if(sexoF.checked) {
+    document.getElementById('result').innerHTML = 'F'
+  }
 }
